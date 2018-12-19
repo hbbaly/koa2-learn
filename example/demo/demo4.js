eb6ec16b5fd9bef4162e16ec5e3c2dde274e6e86
hbbaly <1422699902@qq.com>
2018-12-19 22:15:41 +0800

    ADD: koa-router基本使用方法

--- example/demo/demo4.js
+++ example/demo/demo4.js
@@ -6,6 +6,9 @@ const main = ctx => {
     ctx.response.type = 'html'
     ctx.response.body = '<h1>Index page</h1>'
   } else {
-
+    ctx.response.type = 'text'
+    ctx.response.body = '<h1>text</h1>'
   }
 }
+app.use(main)
+app.listen(3000)
