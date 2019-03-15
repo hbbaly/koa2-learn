aa844834496d79c69c7cd0d6a81053455014ff7a
hbbaly <1422699902@qq.com>
2019-03-15 13:47:37 +0800

    ADD: 添加常用的插件

--- myapp/gulpfile.js
+++ myapp/gulpfile.js
@@ -5,18 +5,26 @@ var rename = require('gulp-rename');
 var miniCss = require('gulp-clean-css')
 var miniImg = require('gulp-imagemin')
 var miniLess = require('gulp-less')
+var autoprefixer = require('gulp-autoprefixer')
+var sourcemaps = require('gulp-sourcemaps')
   // 压缩
   gulp.task('script',function(){
     gulp.src('public/js/*.js')
+    .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('output/js'));
   })
   // css压缩
   gulp.task('css',function(){
     gulp.src('public/css/*.less')
     .pipe(miniLess())
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions'],
+      cascade: false
+    }))
     .pipe(miniCss())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('output/css'))
--- myapp/public/js/jquery.js
+++ myapp/public/js/jquery.js
@@ -1,2 +1,2 @@
 
-console.log('hbv')
+console.log('hbvww')
