e22a9bddf5f880dc19bc232f0f00d0714c58f17b
hbbaly <1422699902@qq.com>
2019-03-15 13:18:06 +0800

    ADD: 压缩css文件

--- myapp/gulpfile.js
+++ myapp/gulpfile.js
@@ -2,17 +2,25 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
+var miniCss = require('gulp-clean-css')
   // 压缩
   gulp.task('script',function(){
     gulp.src('public/js/*.js')
     .pipe(babel())
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('output/'));
+    .pipe(gulp.dest('output/js'));
+  })
+  // css压缩
+  gulp.task('css',function(){
+    gulp.src('public/css/*.css')
+    .pipe(miniCss())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('output/css'))
+    console.log('css文件打包完成');
   })
   // 监控public，routes，views下的文件改动
   gulp.task('auto',function(){
-    gulp.watch('public/js/*.js',['script'])
+    gulp.watch(['public/**/*'],['script','css'])
   })
-  gulp.task('default', ['script', 'auto'])
+  gulp.task('default', ['script','css', 'auto'])
