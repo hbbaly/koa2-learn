2d365764c2ffcb353339bc6d21f30b5e014ff5b1
hbbaly <1422699902@qq.com>
2019-03-15 13:04:43 +0800

    ADD:JS压缩及监听文件的变化

--- myapp/gulpfile.js
+++ myapp/gulpfile.js
@@ -3,10 +3,16 @@ var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-exports.default = function() {
-  return gulp.src(['*.js','routes/**/*.js'])
+  // 压缩
+  gulp.task('script',function(){
+    gulp.src('public/js/*.js')
     .pipe(babel())
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('output/'));
-}
+  })
+  // 监控public，routes，views下的文件改动
+  gulp.task('auto',function(){
+    gulp.watch('public/js/*.js',['script'])
+  })
+  gulp.task('default', ['script', 'auto'])
