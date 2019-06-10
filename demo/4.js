a23a6e35715f12efe74bcd92899921763e598134
hbbaly <1422699902@qq.com>
2019-06-10 14:00:45 +0800

    ADD: update用法

---
+++ demo/4.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  const testSchema = mongoose.Schema({
+    title:String,
+    description: String,
+    url: String,
+    by: String
+  });
+  let Test = mongoose.model('Test', testSchema);
+  // let condition = {title: /^hbb/}
+  let condition1 = {title: '测试用例'}
+  let multi = {
+    multi: true
+  }
+  let update = {$set : {title : 'hbb'}};
+  // Test.update(update, function (err, all) {
+  //   if (err) console.error(srr)
+  //   console.log(all, 'all')  //{ n: 1, nModified: 1, ok: 1 } 'all' 第一个修改完成
+  // })
+  //  Test.update(condition,update, function (err, all) {
+  //   if (err) console.error(srr)
+  //   console.log(all, 'all')  //{ n: 1, nModified: 1, ok: 1 } 'all' 修改已hbb开头的第一个
+  // })
+  Test.update(condition1, update, multi, function (err, all) {
+    if (err) console.error(srr)
+    console.log(all, 'all')  //{ n: 1, nModified: 1, ok: 1 } 'all' 修改已hbb开头的第一个
+    db.close()
+  })
+});
